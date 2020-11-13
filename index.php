5103e53921865e05f4442a0a8e9eda7aa40ead00
rubhermar96 <rubhermar96@gmail.com>
2020-11-13 18:29:25 +0100

    v2

--- index.php
+++ index.php
@@ -53,7 +53,7 @@ echo'
                     <h5>Enlace Heroku </h5>
                     Pulsa sobre esta imagen para ver desplegada la aplicacion sobre heroku
                     <p>
-                    <a title="Heroku" href="https://examenrubenherrera.herokuapp.com/"><img src="imagenes/heroku.png" alt="Heroku" width="120" height="120" /></a>
+                    <a title="Heroku" href="https://examenrubenherrera.herokuapp.com"><img src="imagenes/heroku.png" alt="Heroku" width="120" height="120" /></a>
                     </p>
         </aside>
         <form class="col s12" action="index.php">
